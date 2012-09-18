43b317ed647838634a482240c3391e5d2adbdefe
Liggsie <liggsie@gmail.com>
2012-09-17 21:27:03 -0400

    Added test file and argument support

--- pwgen.rb
+++ pwgen.rb
@@ -4,22 +4,30 @@ require 'securerandom'
 
 class PwGen
   class LengthError < ArgumentError; end
+  class NotNumericError < ArgumentError; end
   attr_reader :length 
 
 
   Lowercase = ('a'..'z').to_a
   Uppercase = ('A'..'Z').to_a
   Numbers = (0..9).to_a
-  Symbols = %w(! @ # $ % ^ & * ( ) - _ = + [ { ] } \ | ; : ' " < , > . ? /)
-  AllChars = Lowercase + Uppercase + Numbers + Symbols
+  Symbols = %q{`~@#%^&*()-_=+[]{}\|;:'",.<>/?}.split //
   DefaultLen = 20
   ValidLengthRange = (6..256)
 
   def initialize(length=DefaultLen)
+    unless _is_number? length
+      raise(NotNumericError, 'Argument must be a valid number!')
+    end
+    length = length.to_i
     unless ValidLengthRange.include? length
       raise(LengthError, 'Length must be within the range %s' % ValidLengthRange.to_s)
     end
     @length = length
+  end
+
+  def _is_number?(val)
+    true if Integer(val) rescue false
   end
 
   def _next
@@ -35,13 +43,17 @@ class PwGen
     @length.times do 
       result << _next
     end
-    result.to_s
+    return result.join
   end
 end
 
 
 def main
-  p = PwGen.new
+  if ARGV.length > 0
+    p = PwGen.new ARGV[0]
+  else
+    p = PwGen.new
+  end
   puts p.pw
 end
 
---
+++ pwgen.spec.rb
@@ -0,0 +1,55 @@
+require './pwgen'
+
+describe PwGen do
+  it 'should have sensible defaults' do
+    PwGen::DefaultLen.should == 20 
+    PwGen::Lowercase.uniq.length.should == 26
+    PwGen::Uppercase.uniq.length.should == 26
+    PwGen::Numbers.uniq.length.should == 10
+    PwGen::Symbols.uniq.length.should == 30
+    PwGen.new.all_chars.length.should == 92
+  end
+
+  it 'should raise on bogus length value' do
+    [nil, 'abc'].each do |value|
+      lambda{PwGen.new value}.should raise_error(PwGen::NotNumericError)  
+    end
+  end
+
+  it 'should raise on an unsuitable default value' do
+    [-1, 0, 3, 5, 257, 400000].each do |value|
+      lambda{PwGen.new value}.should raise_error(PwGen::LengthError)  
+    end
+  end
+
+  it 'should create the object with the appropriate length value' do
+    PwGen.new.length.should == 20
+    PwGen.new(200).length.should == 200
+  end
+
+  it 'should create the password with the appropriate length' do
+    test_obj = PwGen.new
+    test_obj.pw.length.should == PwGen::DefaultLen
+    test_obj = PwGen.new 50
+    test_obj.pw.length.should == 50
+  end
+
+  it 'should give a reasonable distribution among all tokens' do
+    COUNT = 100000
+    test_obj = PwGen.new
+    dict = Hash.new(0)
+    COUNT.times do
+      val = test_obj.send(:_next)
+      dict[val] += 1
+    end
+
+    equal = COUNT / dict.length
+    CONF = 0.9
+    dict.each do |key, value|
+      # Currently I'm checking each value does not differ from count by more than 10%.
+      # TODO: make this more statistically rigorous, with actual confidence intervals.
+      value.should be >= equal * CONF
+      value.should be <= equal / CONF
+    end
+  end
+end
